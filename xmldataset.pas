4706a124d1cfc2d32b1f2db69f0c194f538b1117
alexx <alexx@634ff154-67ff-0310-9183-b4f9e7f8c3cf>
2006-02-07 08:57:59 +0000

    git-svn-id: http://svn.openfmi.net/xmldataset@157 634ff154-67ff-0310-9183-b4f9e7f8c3cf

--- xmldataset.pas
+++ xmldataset.pas
@@ -327,7 +327,6 @@ begin
    else if (FieldType = 'FIXEDCHAR')   then Result := ftFixedChar
    else if (FieldType = 'WIDESTRING')  then Result := ftWideString
    else if (FieldType = 'LARGEINT')    then Result := ftLargeint
-   else if (FieldType = 'INT64')       then Result := ftLargeint // in DB.pp Largeint = Int64
    else if (FieldType = 'ADT')         then Result := ftADT
    else if (FieldType = 'ARRAY')       then Result := ftArray
    else if (FieldType = 'REFERENCE')   then Result := ftReference
@@ -420,7 +419,7 @@ begin
      ftCursor            : ;
      ftFixedChar         : ;
      ftWideString        : ;
-     ftLargeint          : Result := SizeOf(LargeInt); // this is Int64 // todo : check it
+     ftLargeint          : Result := SizeOf(LargeInt);
      ftADT               : ;
      ftArray             : ;
      ftReference         : ;
