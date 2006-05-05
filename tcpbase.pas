31a2f313a5b56baca88ea1547517660407403112
alexx <alexx@634ff154-67ff-0310-9183-b4f9e7f8c3cf>
2006-05-05 12:27:57 +0000

    * unix domain sockets implemented * base class changed * inheritance fixed
    git-svn-id: http://svn.openfmi.net/xmldataset@164 634ff154-67ff-0310-9183-b4f9e7f8c3cf

--- tcpbase.pas
+++ tcpbase.pas
@@ -4,7 +4,9 @@ unit tcpbase;
 
 {*******************************************************************************
 
- Base classes for TCP / IP communications! Use at your own risk!
+ Base classes for TCP/IP and Unix sockets communications!
+ This is a copy from SSockets implementation, but all classes are derived from
+ TTextSocketStream which knows how to handle text.tcpbase Use at your own risk!
 
  (c) 2006 by Alexander Todorov.
  e-mail: alexx.todorov@gmail.com
@@ -25,33 +27,55 @@ interface
 
 uses
   Classes, SysUtils, SSockets;
-  
 
 type
 
-  { TTextInetSocket }
-
-  TTextInetSocket = class(TInetSocket)
+  { TTextSocketStream }
+ 
+  TTextSocketStream = class(TSocketStream)
   public
-    constructor Create(AHandle : Longint); override;
-    
     function  RecvText : String;
     procedure SendText(const AText : String);
   end;
 
+  { TTextInetSocket }
+
+  TTextInetSocket = class(TTextSocketStream)
+  private
+    FHost : String;
+    FPort : Word;
+  protected
+    procedure DoConnect(ASocket : longint); virtual;
+  public
+    constructor Create(ASocket : longint); override; overload;
+    constructor Create(const AHost: String; APort: Word); overload;
+    property Host : String read FHost;
+    property Port : Word read FPort;
+  end;
+
+  {$IFDEF UNIX}
+
+  { TTextUnixSocket }
+  TTextUnixSocket = class(TTextSocketStream)
+  private
+    FFileName : String;
+  protected
+    procedure DoConnect(ASocket : longint); virtual;
+  public
+    constructor Create(ASocket : Longint); overload;
+    constructor Create(AFileName : String); overload;
+    property FileName : String read FFileName;
+  end;
+ {$ENDIF}
+
+
 implementation
 
-uses Sockets;
+uses Sockets, Resolve;
 
-{ TTextInetSocket }
-
-constructor TTextInetSocket.Create(AHandle: Longint);
-begin
-  inherited Create(AHandle);
-  SocketOptions := SocketOptions + [soKeepAlive];
-end;
-
-function TTextInetSocket.RecvText : String;
+{ TTextSocketStream }
+ 
+function TTextSocketStream.RecvText : String;
 var len, n_len, r : LongInt;
 begin
   r := 0;
@@ -59,9 +83,8 @@ begin
   n_len := 0;
   SetLength(Result, 0);
 
-  if (Read(n_len, SizeOf(n_len)) <> SizeOf(n_len)) and
-     (SocketError <> 0) then
-     raise Exception.Create('TTextInetSocket.RecvText - SOCKET ERROR '+IntToStr(SocketError));
+  if (Read(n_len, SizeOf(n_len)) <> SizeOf(n_len)) then
+     raise Exception.Create(ClassName+'.RecvText - SOCKET ERROR '+IntToStr(SocketError));
      
   len := NetToHost(n_len);
   if len > 0 then
@@ -70,14 +93,14 @@ begin
        repeat
          r := r + Read(Result[1+r], len-r);
          if (SocketError <> 0) then
-            raise Exception.Create('TTextInetSocket.RecvText - SOCKET ERROR '+IntToStr(SocketError));
+            raise Exception.Create(ClassName+'.RecvText - SOCKET ERROR '+IntToStr(SocketError));
        until r >= len;
        if r > len then
-          raise Exception.Create('TTextInetSocket.RecvText - more bytes read!');
+          raise Exception.Create(ClassName+'.RecvText - more bytes read!');
      end;
 end;
 
-procedure TTextInetSocket.SendText(const AText: String);
+procedure TTextSocketStream.SendText(const AText: String);
 var len, n_len, w : LongInt;
 begin
   w := 0;
@@ -86,18 +109,86 @@ begin
   
   if (Write(n_len, SizeOf(n_len)) <> SizeOf(n_len)) and
      (SocketError <> 0) then
-     raise Exception.Create('TTextInetSocket.SendText - SOCKET ERROR '+IntToStr(SocketError));
+     raise Exception.Create(ClassName+'.SendText - SOCKET ERROR '+IntToStr(SocketError));
 
   if len > 0 then
      begin
        repeat
          w := w + Write(AText[1+w], len-w);
          if (SocketError <> 0) then
-            raise Exception.Create('TTextInetSocket.SendText - SOCKET ERROR '+IntToStr(SocketError));
+            raise Exception.Create(ClassName+'.SendText - SOCKET ERROR '+IntToStr(SocketError));
        until w = len;
        if w > len then
-          raise Exception.Create('TTextInetSocket.SendText - more bytes read!');
+          raise Exception.Create(ClassName+'.SendText - more bytes read!');
      end;
 end;
+
+{ TTextInetSocket }
+
+procedure TTextInetSocket.DoConnect(ASocket: longint);
+var A : THostAddr;
+    addr: TInetSockAddr;
+begin
+  A := StrToNetAddr(FHost);
+  if A.s_bytes[4] = 0 then
+    with THostResolver.Create(Nil) do
+      try
+        if not NameLookup(FHost) then
+           raise ESocketError.Create(seHostNotFound, [FHost]);
+        A := HostAddress;
+      finally
+        free;
+      end;
+  addr.family := AF_INET;
+  addr.port := ShortHostToNet(FPort);
+  addr.addr := a.s_addr; // hosttonet(A).s_addr;
+//Cardinal(A);
+
+  if not Sockets.Connect(ASocket, addr, sizeof(addr)) then
+    raise ESocketError.Create(seConnectFailed, [Format('%s:%d',[FHost, FPort])]);
+end;
+
+constructor TTextInetSocket.Create(ASocket: Longint);
+begin
+  inherited Create(ASocket);
+  SocketOptions := SocketOptions + [soKeepAlive];
+end;
+
+constructor TTextInetSocket.Create(const AHost: String; APort: Word);
+var S : Longint;
+begin
+  FHost := AHost;
+  FPort := APort;
+  S := Socket(AF_INET,SOCK_STREAM,0);
+  DoConnect(S);
+  inherited Create(S);
+end;
+
+{$IFDEF UNIX}
+
+{ TTextUnixSocket }
+procedure TTextUnixSocket.DoConnect(ASocket: longint);
+var UnixAddr : TUnixSockAddr;
+    AddrLen  : longint;
+begin
+  Str2UnixSockAddr(FFilename,UnixAddr,AddrLen);
+  if not Connect(ASocket,UnixAddr,AddrLen) then
+    raise ESocketError.Create(seConnectFailed,[FFilename]);
+end;
+
+constructor TTextUnixSocket.Create(ASocket: Longint);
+begin
+  inherited Create(ASocket);
+end;
+
+constructor TTextUnixSocket.Create(AFileName: String);
+var S : Longint;
+begin
+  FFileName:=AFileName;
+  S:=Socket(AF_UNIX,SOCK_STREAM,0);
+  DoConnect(S);
+  inherited Create(S);
+end;
+{$ENDIF}
 
 end.
